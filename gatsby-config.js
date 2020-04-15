00e2e203b9ed587e08705afbe690bf13102e8851
Matthew O'Brien <matt.r.obrien@gmail.com>
2020-04-15 15:11:09 -0400

    First blog post using Gatsby plugins, React, and GraphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,28 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-catch-links`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
       },
     },
     `gatsby-transformer-sharp`,
